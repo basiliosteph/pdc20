0edfbc645448866e092f382b10937f342f8f8322
XZYRA LAPID <lapid.xzyra@auf.edu.ph>
2022-10-07 16:14:39 +0800

    Module 4 Activity 1 commit

---
+++ Module04/script.js
@@ -0,0 +1,5 @@
+function ext(){
+    alert('Good afternoon again! This is how to use an external script javascript.');
+    alert('In actuality, this is extremely helpful since you only need to call the function to make it work.')
+    alert('Maybe you could incorporate it in your program as well!')
+}
